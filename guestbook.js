4de100efbc0aa312bd3885eafc5e1fc2c4afed5d
Obira Daniel <obiradaniel@gmail.com>
2022-05-03 11:16:12 +0300

    updated apiURL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://6849559e.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
